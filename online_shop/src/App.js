674f608d461fc700f299c8fa6d1d7fc8590450f9
Beth-Ann Penney <bethann.penney@gmail.com>
2024-08-08 11:53:24 -0230

    Added Footer

--- online_shop/src/App.js
+++ online_shop/src/App.js
@@ -3,13 +3,14 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import ProductDetails from './Components/ProductDetails';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import Footer from './index.css'
 
 function App() {
   return (
     <Router>
-      <div>
-        {/* <Link to="/">Home </Link> |
-        <Link to="/products"> Show Lipgloss Products </Link> | */}
+      <div className="App">
+        <Header />
+        <Footer />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<ProductDetails />} />
