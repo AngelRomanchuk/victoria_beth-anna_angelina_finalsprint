fc6bf66b2afff47a6523f905f2a951034f8ab1f5
Beth-Ann Penney <bethann.penney@gmail.com>
2024-08-08 15:12:26 -0230

    adding Footer

--- online_shop/src/App.js
+++ online_shop/src/App.js
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+ 
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Banner from "./Components/Banner";
-
+import Footer from "./Components/Footer";
+ 
 function App() {
   return (
     <Router>
-      {/* <Header />
+      <Header />
       <Banner
         imageUrl={`${process.env.PUBLIC_URL}/assets/Images/brand-pics/Coming.png`}
-      /> */}
+      />
+      <Footer />
       {/* Define routes if you have other components/pages */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Routes></Routes>
     </Router>
   );
 }
-
+ 
 export default App;
---
+++ online_shop/src/Components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="footer radley-regular-italic">
+         <div className="footer-socials">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="footer-social">Facebook</a>
+          <FaFacebook />
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="footer-social">Instagram</a>
+          <FaInstagram />
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="footer-social">Twitter</a>
+          <FaSquareXTwitter />
+        </div>
+      <p>&copy; 2024 Beauty Bliss. All rights reserved.</p>
+      <div className="footer-links radley-regular-italic">
+        <a href="#faq">FAQ</a>
+        <a href="#privacy policy">Privacy Policy</a>
+        <a href="#contact">Contact</a>
+      </div>
+      <img 
+        src={`${process.env.PUBLIC_URL}/assets/Images/brand-pics/Beauty_Blisspng.png`}
+        className="footerlogo"
+      />
+    </footer>
+  );
+};
+
+export default Footer;
+
